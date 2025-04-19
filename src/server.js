24781ddda98da6bb0a1bd9e58081177cba0584e7
Lukas Martinez <lukasmartinezc@gmail.com>
2025-04-18 23:33:54 -0400

    Set up Express server, database connection, and basic registration route

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import 'dotenv/config'
+import router from './router.js'
+import { connectDB } from './config/db.js'
+
+const app = express()
+
+connectDB()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/', router)
+
+export default app
